73eda72a077e9d9e5ca9afc96cb56180f9be086f
agustiawanrio979@gmail.com <agustiawanrio979@gmail.com>
2024-12-10 06:17:20 -0800

    Delete MODUL-4/12-Latihanmain.py

--- MODUL-4/12-Latihanmain.py
+++
@@ -1,8 +0,0 @@
-tinggi_badan = float(input("masukkan tinggi_badan"))
- berat_badan = float (input("masukkan berat_badan"))
- 
- tinggi_badan = tinggi_badan / 100
- BMI = berat_badan / (tinggi_badan **2)
- 
- print(f"skor bmi adalah : {bmi:1f}")
- 
